6681a3b0339621227f08d19cfd9cf367859aecae
Jesper Brix Rosenkilde <jesperb@assurator.dk>
2016-05-29 20:29:01 +0200

    Backup script first commit

---
+++ backup-emojis.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+TOKEN=$(cat token)
+BACKUPFOLDER=emojis
+mkdir -p $BACKUPFOLDER
+
+emojiList=$(curl -s "https://slack.com/api/emoji.list?token=$TOKEN" | jq '.emoji')
+
+for e in $(jq 'keys|@sh' <<< $emojiList | tr -d \' | tr -d \"); do
+  url=$(jq -r ".[\"$e\"]" <<< $emojiList)
+  if grep -q -E '^alias:' <<< $url; then
+    #alias
+    echo $url
+    file=$BACKUPFOLDER/${e}.alias
+    sed -e 's/^alias:\(.*\)/\1/' <<< $url > $file
+  else
+    continue
+    #url
+    suffix=${url##*.}
+    file=$BACKUPFOLDER/${e}.$suffix
+    if [ -f $file ]; then
+      curl -s -o $file -z $file $url
+    else
+      curl -s -o $file $url
+    fi
+  fi
+done
