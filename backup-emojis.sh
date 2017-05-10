cb178c71afa52ad37be1d941d3592552cb7887db
Jesper B. Rosenkilde <jbr@humppa.dk>
2017-05-10 17:13:30 +0200

    lint with shellcheck

--- backup-emojis.sh
+++ backup-emojis.sh
@@ -8,17 +8,17 @@ mkdir -p $BACKUPFOLDER
 
 emojiList="$(curl -s "https://slack.com/api/emoji.list?token=$TOKEN" | jq '.emoji')"
 
-for e in $(jq 'keys|@sh' <<< $emojiList | tr -d \' | tr -d \"); do
-  url=$(jq -r ".[\"$e\"]" <<< $emojiList)
-  if grep -q -E '^alias:' <<< $url; then
+for e in $(jq 'keys|@sh' <<< "$emojiList" | tr -d \' | tr -d \"); do
+  url=$(jq -r ".[\"$e\"]" <<< "$emojiList")
+  if grep -q -E '^alias:' <<< "$url"; then
     #alias
     file=$BACKUPFOLDER/${e}.alias
-    sed -e 's/^alias:\(.*\)/\1/' <<< $url > $file
+    sed -e 's/^alias:\(.*\)/\1/' <<< "$url" > "$file"
   else
     #url
     suffix=${url##*.}
     file=$BACKUPFOLDER/${e}.$suffix
-    emoji=$(basename $file | cut -f 1 -d '.')
+    emoji=$(basename "$file" | cut -f 1 -d '.')
     if [ -f "$file" ]; then
       curl -s -o "${file}.tmp" "$url"
       if $(cmp --silent "${file}.tmp" "$file"); then
